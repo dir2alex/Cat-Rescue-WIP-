ac648c8b5e53b8326715fe0333ac19c68e04b025
dir2alex <91107711+dir2alex@users.noreply.github.com>
2024-05-15 10:08:46 +0700

    Add files via upload
    game file is > 25 MB so i will upload it in a drive folder

---
+++ menu.gd
@@ -0,0 +1,21 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("game is ready")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_pressed():
+	print("entering game")
+	get_tree().change_scene_to_file("res://tutorial.tscn")
+
+
+func _on_quit_pressed():
+	print("bye bye!")
+	get_tree().quit()
